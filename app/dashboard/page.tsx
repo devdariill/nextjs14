7e6e48588de7bba453235fc691734c309d66e440
devdariill <93483481+devdariill@users.noreply.github.com>
2023-11-02 22:40:34 +0000

    * Suspense fallback loading... *

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,12 @@
-import { fetchLatestInvoices, fetchRevenue } from "../lib/data";
+import { Suspense } from "react";
+import { fetchLatestInvoices } from "../lib/data";
 import LatestInvoices from "../ui/dashboard/latest-invoices";
 import RevenueChart from "../ui/dashboard/revenue-chart";
 import { lusitana } from "../ui/fonts";
 
 async function Page() {
-  const revenue = await fetchRevenue();
+
   const latestInvoices = await fetchLatestInvoices();
-  console.log(revenue);
   return (
     <main>
       <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
@@ -23,7 +23,9 @@ async function Page() {
       /> */}
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+        <Suspense fallback={<div>Cargando ...</div>}>
+          <RevenueChart />
+        </Suspense>
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
