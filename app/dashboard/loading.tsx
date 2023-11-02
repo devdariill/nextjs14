c8c3271c56d4318dc22638be40be2f734be29994
devdariill <93483481+devdariill@users.noreply.github.com>
2023-11-02 22:37:26 +0000

    skeleton loader

--- app/dashboard/loading.tsx
+++ app/dashboard/loading.tsx
@@ -1,1 +1,3 @@
-export default function Loading() { return <div>Loading...</div> }
+import DashboardSkeleton from "../ui/skeletons";
+
+export default function Loading() { return <DashboardSkeleton/> }
